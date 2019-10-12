fcd3667f9bf0d724f2a371b40a17b32da20cfa4e
phumelelajuliet <phumelelamagagula@yahoo.com>
2019-10-12 22:42:57 +0200

    datatables

--- Site/neverendingfintechies/mainnode.js
+++ Site/neverendingfintechies/mainnode.js
@@ -1,3 +1,8 @@
+require( 'datatables.net-dt' )();
+require( 'datatables.net-buttons-dt' )();
+require( 'datatables.net-keytable-dt' )();
+require( 'datatables.net-responsive-dt' )();
+require( 'datatables.net-select-dt' )();
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
@@ -6,11 +11,19 @@ var con = mysql.createConnection({
   password: "DemoPassword4*",
   database: "fintechdb"
 });
-//pull data from database
-con.connect(function(err) {
-    if (err) throw err;
-    con.query("SELECT * FROM book", function (err, result, fields) {
-      if (err) throw err;
-      console.log(result);
+
+var dataSet= ["1","2","3","4","5","6"]
+  //table data document
+  $(document).ready(function() {
+    $('#bookPurchase').DataTable({
+      data: dataSet,
+        columns: [
+            { title: "ISBN13" },
+            { title: "ISBN10" },
+            { title: "Author" },
+            { title: "Edition" },
+            { title: "Publisher" },
+            { title: "Published" }
+        ]
     });
-  });
+} );
